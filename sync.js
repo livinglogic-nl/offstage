14a9fe728433d864f0deef8efd0e1c1f8f27446c
Rinke van den Berg <rinke.vandenberg@pidz.nl>
2022-10-04 19:52:09 +0200

    simplify

--- sync.js
+++ sync.js
@@ -4,28 +4,9 @@ const esbuild = require('esbuild');
 const rootDir = process.cwd();
 const sourceDir = `${rootDir}/src`;
 
-const typescriptFile = `${rootDir}/generator.ts`;
-const javascriptFile = `${rootDir}/generator.js`;
+const { generate } = require('./index.js');
 
-// 1. generate a typescript file that:
-//  - imports the mock
-//  - generates the api
-const generateTypescriptFile = async() => {
-  await fs.promises.writeFile(typescriptFile, `
-import fs from 'fs';
-import { generate } from 'offstage/index.js';
-
-(async() => {
-  await import('${sourceDir}/offstage/mock.ts');
-  const api = generate();
-  fs.writeFileSync('${sourceDir}/offstage/index.ts', api);
-})();
-`);
-
-}
-
-// 2. convert the typescript file to javascript
-const convertToJavascript = async() => {
+const convertMockToJavascript = async() => {
   await esbuild.build({
     entryPoints: ['src/offstage/mock.ts'],
     platform: 'node',
@@ -34,26 +15,12 @@ const convertToJavascript = async() => {
     format: 'cjs',
     outfile: 'src/offstage/mock.js',
   });
-  await esbuild.build({
-    entryPoints: [typescriptFile],
-    platform: 'node',
-    bundle: true,
-    external: [ 'offstage' ],
-    format: 'cjs',
-    outfile: javascriptFile,
-  });
 }
 
-// 3. execute the javascript file
-const executeJavascript = async() => {
-  require(javascriptFile);
+const writeApiFile = async() => {
+  await import(rootDir + '/src/offstage/mock.js')
+  fs.writeFileSync(`${sourceDir}/offstage/index.ts`, generate());
 }
 
-
-(async() => {
-  await generateTypescriptFile();
-  await convertToJavascript();
-  await executeJavascript();
-})();
-
+convertMockToJavascript().then(writeApiFile);
 
