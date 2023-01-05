e3dd32cd7d8316e9187e2bc251b5c44a625ba118
Rinke van den Berg <rinke.vandenberg@pidz.nl>
2023-01-05 13:34:04 +0100

    fix: cacheSeconds key hash as hex

--- src/use-endpoint.ts
+++ src/use-endpoint.ts
@@ -104,9 +104,16 @@ export default (state:OffstageState) => {
     return resultData.result;
   }
 
+  const digestMessage = async(message:string) => {
+    const msgUint8 = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+  }
+
   const calculateKey = async(requestData:any) => {
-    const encoded = (new TextEncoder()).encode(JSON.stringify(requestData));
-    return 'offstage-' + await crypto.subtle.digest('sha-1', encoded);
+    return 'offstage-' + digestMessage(JSON.stringify(requestData));
   }
 
   const loadCache = async(config:OffstageConfig, requestData:any) => {
