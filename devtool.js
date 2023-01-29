d6bfdf8869b2d7727633720118e552e3060a98b5
Rinke van den Berg <rinke.vandenberg@pidz.nl>
2023-01-29 18:55:39 +0100

    fix: shadow dom devtool

--- devtool.js
+++ devtool.js
@@ -1,159 +1,33 @@
-
-const logoContent = () => `
-<svg viewBox="0 0 64 64" height="16">
-<g transform="translate(0,44)"><rect width="60" height="20" fill="yellow" rx="3" /></g>
-<g transform="translate(0,22)"><rect width="40" height="20" fill="#f60" rx="2" /><path d="M0,20l0,4 42,0 -1,-4z" fill="black" /></g>
-<g><rect width="20" height="20" fill="#e00" rx="1" /><path d="M0,20l0,4 22,0 -1,-4z" fill="black" /></g>
-</svg>
-`;
-
-(() => {
+const build = () => {
   const { offstage } = window;
-
-  if(offstage.devtool) { return ; }
-  offstage.devtool = 1;
-
-  const root = document.createElement('div');
-  Object.assign(root.style, {
-    position: 'fixed',
-    bottom: '0',
-    left: '20px',
-    cursor: 'pointer',
-  });
-  document.body.appendChild(root);
-
   const state = {
     menuOpen: false,
     filters: [],
   }
+  const host = document.createElement('div');
+  Object.assign(host.style, {
+    position: 'fixed',
+    bottom: '0',
+    left: '20px',
+  });
+  const shadow = host.attachShadow({ mode:'closed' });
 
-  const isForceNetwork = () => offstage.forceNetwork || window.isOffstagePlaywright;
+  const style = document.createElement('style');
+  style.textContent = styleContent();
+  shadow.appendChild(style);
 
-  const renderHistory = content => {
-    const codeStyle = {
-      whiteSpace: 'pre',
-      fontSize: '12px',
-      lineHeight: '1.3em',
-      borderBottom: '1px solid #222',
-      padding: '8px 0',
+  const root = document.createElement('div');
+  root.className = 'root';
+  shadow.appendChild(root);
+
+  const el = (parent, tag, classNames, content, clickHandler) => {
+    const div = document.createElement(tag);
+    if(classNames) {
+      div.className = classNames.join(' ');
     }
-    offstage.history
-      .filter(({ serviceMethodName }) =>
-        state.filters.length === 0 || state.filters.includes(serviceMethodName)
-      )
-      .forEach(entry => {
-      const div = el(content, '', {});
-      el(div,`
-        <span style="color: #f60">${entry.serviceMethodName}</span>
-        <span style="color: #888">${entry.date.toLocaleString()}</span>
-      `, {
-          display: 'flex',
-          justifyContent: 'space-between',
-        }),
-      el(div, entry.endpoint, { fontWeight:'bold' }),
-      el(div, JSON.stringify(entry.requestData, null, 4), codeStyle);
-      el(div, JSON.stringify(entry.responseData, null, 4), codeStyle);
-    });
-  }
-
-  const renderLogFilter = log => {
-    const filter = el(log, '', {
-      flex: 1,
-      padding: '10px',
-      borderLeft: '1px solid #222',
-    });
-    
-    Object.entries(offstage.services).forEach(([serviceName, service]) =>
-      Object.keys(service).forEach(methodName => {
-        const key = `${serviceName}.${methodName}`
-
-        const isActive = state.filters.includes(key);
-        checkbox(filter, key, {opacity: isActive ? 1 : 0.6}, isActive, (on) => {
-          state.filters = on
-            ?
-            [...state.filters, key]
-            :
-            state.filters.filter(k => k != key)
-        })
-      })
-    );
-  }
-
-  const renderLog = root => {
-    const log = el(root, '', {
-      background: 'rgba(0,0,0,0.8)',
-      border: '1px solid #222',
-      position: 'fixed',
-      width: '800px',
-      height: isForceNetwork() ? 'auto' : '400px',
-      maxWidth: 'calc(100vw - 64px)',
-      maxHeight: 'calc(100vh - 64px)',
-      bottom: '31px',
-      display: 'flex',
-    });
-
-    const content = el(log, '', {
-      flex: 3,
-      padding: '10px',
-      overflow: 'auto',
-      fontFamily: 'monospace',
-    });
-
-    if(isForceNetwork()) {
-      el(content, 'Force network enabled. Actual HTTP traffic is visible in your browsers devtools');
-    } else {
-      renderHistory(content);
-      renderLogFilter(log);
+    if(content) {
+      div.innerHTML = content;
     }
-
-  }
-
-  const renderMenu = (root) => {
-    const menu = el(root, '', {
-      position: 'absolute',
-      bottom: '0px',
-      left: '32px',
-    });
-
-    
-    checkbox(menu, 'Force network', {}, isForceNetwork(),  () => {
-      offstage.forceNetwork = !offstage.forceNetwork;
-      state.menuOpen = false;
-    });
-  }
-
-  const render = () => {
-    root.innerHTML = '';
-    const borderRadius = '4px';
-    el(root, logoContent(), {
-      width: '32px',
-      height: '32px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      background: '#111',
-      boxSizing: 'border-box',
-      border: '1px solid white',
-      borderBottom: 0,
-      borderTopLeftRadius: borderRadius,
-      borderTopRightRadius: borderRadius,
-      border: isForceNetwork()
-        ? '2px solid blue'
-        : '1px solid rgba(255,255,255,0.1)',
-    }, () => {
-      state.menuOpen = !state.menuOpen;
-    });
-
-    if(state.menuOpen) {
-      renderMenu(root);
-      renderLog(root);
-    }
-  }
-
-  const el = (parent, content, style, clickHandler) => {
-    const div = document.createElement('div');
-    div.innerHTML = content;
-    Object.assign(div.style, style);
     if(clickHandler) {
       div.onclick = () => {
         clickHandler();
@@ -163,36 +37,84 @@ const logoContent = () => `
     parent.appendChild(div);
     return div;
   }
-  const checkbox = (parent, content, style, isActive, changeHandler) => {
-    const label = document.createElement('label');
-    label.innerHTML = `<input type="checkbox" ${isActive ? 'checked' : ''} /> ${content}`;
-    Object.assign(label.style, {
-      height: '32px',
-      display: 'flex',
-      alignItems: 'center',
-      padding: '0 8px',
-      whiteSpace: 'nowrap',
-      ...style,
-    });
-    label.querySelector('input').onchange = (e) => {
+
+  const checkbox = (parent, classNames, content, isActive, changeHandler) => {
+    const label = el(parent, 'label', classNames);
+
+    const input = el(label, 'input');
+    el(label, 'span', [], content);
+
+    input.type = 'checkbox';
+    input.checked = isActive;
+    input.onchange = (e) => {
       changeHandler(e.target.checked);
       render();
     }
-    parent.appendChild(label);
     return label;
   }
 
-  render();
-  offstage.onHistoryUpdate = render;
+  const isForceNetwork = () => offstage.forceNetwork || window.isOffstagePlaywright;
 
+  const render = () => {
+    root.innerHTML = '';
+    root.className = isForceNetwork() ? 'root force' : 'root';
+    el(root, 'div', [ 'logo' ], logoContent(), () => {
+      state.menuOpen = !state.menuOpen;
+    });
+
+    if(state.menuOpen) {
+      const menu = el(root, 'div', [ 'menu' ]);
+      checkbox(menu, [ 'option' ], 'Force network', isForceNetwork(), () => {
+        offstage.forceNetwork = !offstage.forceNetwork;
+        state.menuOpen = false;
+      });
+
+      const log = el(root, 'div', [ 'log' ]);
+
+      if(isForceNetwork()) {
+        el(log, 'div', [ 'content' ], 'Force network enabled. Actual HTTP traffic is visible in your browsers devtools');
+      } else {
+        const content = el(log, 'div', [ 'content' ]);
+        offstage.history
+          .filter(({ serviceMethodName }) =>
+            state.filters.length === 0 || state.filters.includes(serviceMethodName)
+          )
+          .forEach(entry => {
+             el(content, 'div', [ 'entry' ], `
+                <div class="call">
+                  <span class="service-method">${entry.serviceMethodName}</span>
+                  <span class="date-time">${entry.date.toLocaleString()}</span>
+                </div>
+                <div class="endpoint">${entry.endpoint}</div>
+                <div class="request">${JSON.stringify(entry.requestData, null, 2)}</div>
+                <div class="response">${JSON.stringify(entry.responseData, null, 2)}</div>
+            `);
+          })
+        const filter = el(log, 'div', [ 'filter' ]);
+        Object.entries(offstage.services).forEach(([serviceName, service]) =>
+          Object.keys(service).forEach(methodName => {
+            const key = `${serviceName}.${methodName}`
+            const count = offstage.history.filter(e => e.serviceMethodName === key).length;
+
+            const isActive = state.filters.includes(key);
+            checkbox(filter, [], `${key} (${count})`, isActive, on => {
+              state.filters = on ? [...state.filters, key] : state.filters.filter(k => k != key);
+            });
+          })
+        );
+      }
+    }
+  }
+  offstage.onHistoryUpdate = render;
+  render();
   let visible = false;
   const setVisible = to => {
     if(visible === to) { return; }
     visible = to;
     if(visible) {
-      document.body.appendChild(root);
+      document.body.appendChild(host);
     } else {
-      document.body.removeChild(root);
+      document.body.removeChild(host);
     }
     render();
   }
@@ -203,5 +125,126 @@ const logoContent = () => `
     setVisible(!window.isOffstagePlaywright);
   }
   frame();
+}
 
-})();
+const styleContent = () => `
+
+.root {
+  all: inherit;
+  color: white;
+  --seperator-color: rgba(255,255,255,0.2);
+  --seperator-color: #222;
+}
+
+.logo {
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #111;
+  box-sizing: border-box;
+  border: 1px solid white;
+  border-bottom: 0;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+
+.force .logo {
+  border-color: green;
+}
+
+.logo {
+  border-color: rgba(255,255,255,0.2);
+}
+
+.menu {
+  position: absolute;
+  bottom: 0px;
+  left: 32px;
+}
+
+label {
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  height: 32px;
+}
+
+.menu label {
+  background: #111;
+  color: white;
+  padding: 0 10px;
+}
+
+.log {
+  background: #111;
+  border: 1px solid #222;
+  position: fixed;
+  width: 800px;
+  height: 400px;
+  max-width: calc(100vw - 64px);
+  max-height: calc(100vh - 64px);
+  bottom: 32px;
+  display: flex;
+  font-size: 14px;
+  line-height: 1.2em;
+}
+
+.force .log {
+  height: auto;
+}
+
+.log .content {
+  flex: 3;
+  padding: 10px;
+  overflow: auto;
+  font-family: monospace;
+}
+
+
+.entry {
+  border-bottom: 1px solid var(--seperator-color);
+}
+.entry>div {
+  padding: 4px 0;
+}
+
+.call {
+  display: flex;
+  justify-content: space-between;
+}
+
+.service-method {
+  color: #f60;
+}
+
+.date-time {
+  font-size: 12px;
+}
+
+.request, .response {
+  font-size: 12px;
+  border-top: 1px solid var(--seperator-color);
+  white-space: pre;
+}
+
+.log .filter {
+  flex: 1;
+  padding: 10px;
+  border-left: 1px solid #222;
+}
+
+`;
+const logoContent = () => `
+  <svg viewBox="0 0 64 64" height="16">
+    <g transform="translate(0,44)"><rect width="60" height="20" fill="yellow" rx="3" /></g>
+    <g transform="translate(0,22)"><rect width="40" height="20" fill="#f60" rx="2" /><path d="M0,20l0,4 42,0 -1,-4z" fill="black" /></g>
+    <g><rect width="20" height="20" fill="#e00" rx="1" /><path d="M0,20l0,4 22,0 -1,-4z" fill="black" /></g>
+  </svg>
+`;
+
+
+build();
+
