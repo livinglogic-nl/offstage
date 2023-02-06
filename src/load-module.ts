0f06b040c448b896cb7ec6971c97695adf5832e6
Rinke van den Berg <rinke.vandenberg@pidz.nl>
2023-02-06 11:32:10 +0100

    fix: update cached service mocks when necessary

--- src/load-module.ts
+++ src/load-module.ts
@@ -11,7 +11,6 @@ const getFileHash = async(file:string) => {
 }
 
 const transformFile = async(file:string, transFile:string) => {
-  // const format = global['require'] === undefined ? 'esm' : 'cjs';
   const format = 'cjs';
   esbuild.buildSync({
     bundle: true,
@@ -23,12 +22,18 @@ const transformFile = async(file:string, transFile:string) => {
   });
 }
 
+
+const fileIsNewer = async(a:string,b:string) => {
+  const [as,bs] = await Promise.all([ fs.promises.stat(a), fs.promises.stat(b) ]);
+  return new Date(as.mtime) > new Date(bs.mtime);
+}
+
 export default async(file:string) => {
   let result = transformed[file];
   if(!result) {
     const hash = await getFileHash(file);
     const transFile = `${process.cwd()}/node_modules/.offstage/${hash}.js`;
-    if(!fs.existsSync(transFile)) {
+    if(!fs.existsSync(transFile) || await fileIsNewer(file, transFile)) {
       await transformFile(file, transFile);
     }
     result = transformed[file] = transFile;
